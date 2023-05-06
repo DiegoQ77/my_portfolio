edd5532e6056f719c186cfe66912c7fd60e29690
DiegoQ77 <diego.quisbert77@gmail.com>
2023-05-05 22:05:29 -0400

    version one

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
 import code from "../../public/logo.png";
 
 function NavBar() {
@@ -18,20 +20,21 @@ function NavBar() {
       style={{ position: "fixed", zIndex: 9999, width: "100%" }}
     >
       <div className="container flex flex-wrap items-center justify-between mx-auto">
-        <a href="#" className="flex items-center">
+        <Link to="/" className="flex items-center">
           <img src={code} alt="" className="h-6 mr-3 sm:h-9" />
           <span className="self-center text-xl font-bold whitespace-nowrap dark:text-white">
             My Portfolio
           </span>
-        </a>
+        </Link>
+
         <div className="flex md:order-2">
-          <a
-            href="/articles"
+          <Link
+            to="/articles"
             type="button"
             className="text-white bg-blue-primary hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-bold rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             My Articles
-          </a>
+          </Link>
           <button
             data-collapse-toggle="navbar-cta"
             type="button"
@@ -56,27 +59,37 @@ function NavBar() {
             </svg>
           </button>
         </div>
+
         <div
           className={`items-center justify-between  w-full md:flex md:w-auto md:order-1 ${hidden}`}
           id="navbar-cta"
         >
           <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-bold md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-              <a
-                href="/"
+              <Link
+                to="/"
                 className="block py-2 pl-3 pr-4 text-gray-700 bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
                 aria-current="page"
               >
                 Home
-              </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="/articles"
+              <Link
+                to="/articles"
                 className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 Articles
-              </a>
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                to="/articles"
+                className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                My Projects
+              </Link>
             </li>
           </ul>
         </div>
